ce9d016435da5a2d0c044f6642406b4ced2fc74f
Enoch Abiodun <ezea128@gmail.com>
2025-06-29 03:06:56 +0100

    Attempt1

--- lua/ssh_launcher/init.lua
+++ lua/ssh_launcher/init.lua
@@ -2,11 +2,11 @@ local M = {}
 
 function M.setup()
     vim.api.nvim_create_user_command("SshLauncher", function()
-        require("sshlauncher.launcher").launch_ssh()
+        require("ssh_launcher.launcher").launch_ssh()
     end, {})
 
     vim.api.nvim_create_user_command("SshAddKey", function()
-        require("sshlauncher.addkey").add_key()
+        require("ssh_launcher.addkey").add_key()
     end, {})
 end
 
